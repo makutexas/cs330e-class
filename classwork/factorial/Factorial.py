2a0360eb2c5b5017f1d2d4fcdada1f6fb86bb7c0
Mark Chao <mark.chao@utexas.edu>
2023-09-20 11:57:24 -0500

    Added Factorial Classwork

---
+++ classwork/factorial/Factorial.py
@@ -0,0 +1,16 @@
+from math import factorial
+
+def factorial_range_for():
+    return
+
+def factorial_range_reduce():
+    return
+
+def factorial_recursion():
+    return
+
+def factorial_tail_recursion():
+    return
+
+def factorial_while():
+    return
